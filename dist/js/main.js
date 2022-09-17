cc59188e92efb36ef3d46d66a9b206120508b4f6
Smruti Ranjan Nayak <56423859+SM8UTI@users.noreply.github.com>
2022-09-17 18:25:08 +0530

    responsive hero section addedd and something update in header code

--- dist/js/main.js
+++ dist/js/main.js
@@ -16,3 +16,36 @@ navMenu.forEach((e)=>{
         e.classList.toggle("active")
     })
 })
+
+
+//Count Down  timer 
+
+const offerLastDate = '20 Oct 2022';
+
+const days = document.getElementById('days');
+const hours = document.getElementById('hours');
+const minutes = document.getElementById('minutes');
+const seconds = document.getElementById('seconds');
+
+const countDownTimer = () =>{
+    const lastDate = new Date(offerLastDate);
+    const currentDate = new Date();
+
+    const totalSeconds = (lastDate - currentDate)/1000;
+
+    const day = Math.floor(totalSeconds/3600/24)
+    const hour = Math.floor(totalSeconds/3600%24)
+    const minute = Math.floor(totalSeconds/60%60)
+    const second = Math.floor(totalSeconds%60)
+
+    days.innerText = formatTime(day);
+    hours.innerText = formatTime(hour);
+    minutes.innerText = formatTime(minute);
+    seconds.innerText = formatTime(second);
+};
+
+const formatTime = (time) => {
+    return time < 10 ? (`0${time}`) : time;
+}
+
+setInterval(countDownTimer,1000);
