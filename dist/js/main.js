a20871d1a12fa735226c078caac31e8566646374
Smruti Ranjan Nayak <56423859+SM8UTI@users.noreply.github.com>
2022-09-20 22:37:58 +0530

    responsive testomonial section added

--- dist/js/main.js
+++ dist/js/main.js
@@ -49,7 +49,23 @@ const priceFeatures = [
     },
 ]
 
-
+const reviews = [
+    {
+        name:"Colin Hamilton",
+        message:"Hostinger is one of the best web hosting companies I've had the opportunity to work with in some time. With its complete services and pre setup environment for WordPress, Hostinger is an amazing company to work with.",
+        image:'https://images.unsplash.com/photo-1609017879802-fd6e6fb563bd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
+    },
+    {
+        name:"Britain Eriksen",
+        message:"As a customer of hostinger in Indonesia, I am one of their early customers. I am very amazed by the service updates and features that always increase from time to time with competitive prices, safe and always up to date. Thank you hostinger as the best hosting company.",
+        image:'https://images.unsplash.com/profile-1603267077654-3d2d911647f7image?dpr=1&auto=format&fit=crop&w=150&h=150&q=60&crop=faces&bg=fff',
+    },
+    {
+        name:"Allef Vinicius",
+        message:"I've been using Hostinger for a couple of years now and I'm very satisfied.I'm using it both for WordPress websites on a shared server and for NodeJs apps on a VPS server.",
+        image:'https://images.unsplash.com/photo-1547395267-9900968de5a7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
+    },
+]
 
 const header = document.getElementById('header');
 const hamMenu = document.getElementById('hamburger-menu');
@@ -104,6 +120,9 @@ const formatTime = (time) => {
 setInterval(countDownTimer,1000);
 
 
+
+// price list section 
+
 const priceFetureLists = document.querySelector('.price-container-box-card-list')
 
 for(let i = 0;i<priceFeatures.length;i++){
@@ -111,4 +130,20 @@ for(let i = 0;i<priceFeatures.length;i++){
                             <img src="assets/vector/correct.svg" alt="">
                             <p>${priceFeatures[i].features}</p>
                         </li>`
+}
+
+
+// reviews section 
+
+const testomonialBox = document.querySelector('.testomonial-container-box')
+
+for(let i = 0;i<reviews.length;i++){
+    testomonialBox.innerHTML += `<div class="testomonial-container-box-item swiper-slide">
+                        <img src="assets/vector/message-cl.svg" alt="">
+                        <p>${reviews[i].message}</p>
+                        <div class="testomonial-container-box-item-profile">
+                            <img src="${reviews[i].image}" alt="">
+                            <h2>${reviews[i].name}</h2>
+                        </div>
+                    </div>`
 }
